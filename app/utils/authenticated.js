9e1a63cd7403dfaaad755bfcf93137c3c3b7b2c4
Daniel Hoang <thesubway@gmail.com>
2015-03-11 20:26:18 -0600

    Update the authenticated file to redirect user's logins.

--- app/utils/authenticated.js
+++ app/utils/authenticated.js
@@ -2,7 +2,15 @@ var Login = require("../components/login-register/Login");
 var firebaseUtils = require('./firebaseUtils');
 
 var Authenticated = {
-
+	statics: {
+		willTransitionTo: function(transition){
+			//where we store where the user was trying to go.
+			if (!firebaseUtils.isLoggedIn()) {
+				Login.attemptedTransition = transition;
+				transition.redirect('login');
+			}
+		}
+	}
 };
 
 module.exports = Authenticated;
